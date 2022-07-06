3cd89ec1e210dfa8499ae1f06cdb999213bf8c82
mhderdnsyh <1205011053@students.uin-suska.ac.id>
2022-07-06 16:20:11 +0700

    first commit

---
+++ app/src/main/java/com/nsh/goapps/config/ServerConfig.java
@@ -0,0 +1,7 @@
+package com.nsh.goapps.config;
+
+public class ServerConfig {
+    public static final String SERVER_URL = "http://172.16.143.252/goApps/";
+    public static final String API_ENDPOINT = SERVER_URL;
+
+}
